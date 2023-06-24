82f55fa602d00a11ab0be3caa4f457c3322f01ba
nik-ubuntu <nik-ubuntu@My-ubuntu.myguest.virtualbox.org>
2023-06-24 10:18:45 -0600

    making names match

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,19 +6,19 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-AESD_CHAR_DRIVER_VERSION = '3797ba6418cad3bb568050c2a61762860a872129'
+AESDCHAR_DRIVER_VERSION = '3797ba6418cad3bb568050c2a61762860a872129'
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-nmilligan-cu.git
-AESD_CHAR_DRIVER_SITE_METHOD = git
-AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
-AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-nmilligan-cu.git
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 
 $(eval $(kernel-module))
 
-define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+define AESDCHAR_INSTALL_TARGET_CMDS
 	mkdir -p $(BR2_ROOTFS_OVERLAY)/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(BR2_ROOTFS_OVERLAY)/bin/aesdchar_load
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(BR2_ROOTFS_OVERLAY)/bin/aesdchar_unload
