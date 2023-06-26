bcd2000e6d8def57a73032772f306ea042605aa8
nik-ubuntu <nik-ubuntu@My-ubuntu.myguest.virtualbox.org>
2023-06-25 20:34:59 -0600

    init newpos

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'bb8dfc4884b692fb62a01da9d108574d40d7cc3d'
+AESD_ASSIGNMENTS_VERSION = '794dfdd83ba2388a52d05d177f867a2499fc9875'
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
