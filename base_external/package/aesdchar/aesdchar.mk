1fa246b46a28dc8c1f1e0b6ae065f7fbc59efc4a
nik-ubuntu <nik-ubuntu@My-ubuntu.myguest.virtualbox.org>
2023-06-25 19:06:39 -0600

    debug

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-AESDCHAR_VERSION = '18ee464d3abd5e1eaedc836827cb5bdc48e1f903'
+AESDCHAR_VERSION = 'a7213d229c48de6ace62bb2d8ef02de10bacb252'
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
